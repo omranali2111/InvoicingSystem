91a50415c98367dfc6389d5699b7a327d4d9deeb
omranali2111 <aljabri1234@hotmail.com>
2023-08-07 10:24:39 +0400

    adding shop item class and Manage shop item class to perform  methods related to the shop item using composition relation between the two classes

---
+++ InvoicingSystem/ManageShopItems.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoicingSystem
+{
+    internal class ManageShopItems
+    {
+        private List<ShopItem> shopInventory = new List<ShopItem>();
+
+        public void addItems()
+        {
+
+
+        }
+        public void deleteItems()
+        {
+
+        }
+        public void changeItemsPrice()
+        {
+
+        }
+        public void reportAllItems()
+        {
+
+        }
+
+
+    }
+}
