bf768e1c7416fb294f1e6597d031fa83ad7b1b05
omranali2111 <aljabri1234@hotmail.com>
2023-08-07 10:39:00 +0400

    implementing  addItem method and adding new method to Serializer the list and save it to filePath( .NET framework's built-in serialization)

--- InvoicingSystem/ManageShopItems.cs
+++ InvoicingSystem/ManageShopItems.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace InvoicingSystem
@@ -10,10 +11,15 @@ namespace InvoicingSystem
     {
         private List<ShopItem> shopInventory = new List<ShopItem>();
 
-        public void addItems()
+        public void addItems(ShopItem item)
         {
+            shopInventory.Add(item);
 
-
+        }
+        public void SaveInventoryToFile(string filePath)
+        {
+            string json = JsonSerializer.Serialize(shopInventory);
+            File.WriteAllText(filePath, json);
         }
         public void deleteItems()
         {
