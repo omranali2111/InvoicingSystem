5c9b269977ec8eddb46a421e0d81eec09d50052b
omranali2111 <aljabri1234@hotmail.com>
2023-08-07 12:17:40 +0400

    adding InvoiceNumber and AddItem method

--- InvoicingSystem/Invoice.cs
+++ InvoicingSystem/Invoice.cs
@@ -8,6 +8,8 @@ namespace InvoicingSystem
 {
     internal class Invoice
     {
+        private static int _lastInvoiceNumber = 0;
+        public string InvoiceNumber { get; }
         public string CustomerFullName { get; set; }
         public string PhoneNumber { get; set; }
         public DateTime InvoiceDate { get; set; }
@@ -15,6 +17,41 @@ namespace InvoicingSystem
         public decimal PaidAmount { get; set; }
         public decimal TotalAmount => Items.Sum(item => item.Price * item.Quantity);
         public decimal Balance => TotalAmount - PaidAmount;
+
+        public Invoice()
+        {
+            _lastInvoiceNumber++;
+            InvoiceNumber = $"INV{_lastInvoiceNumber:D6}"; // Format invoice number as INV000001, INV000002, etc.
+        }
+
+        public void AddItem(ShopItem item, int quantity = 1)
+        {
+            try
+            {
+                if (quantity <= 0)
+                {
+                    throw new ArgumentException("Quantity must be greater than zero.");
+                }
+
+                // Check if the item is already added to the invoice
+                var existingItem = Items.FirstOrDefault(i => i.ItemId == item.ItemId);
+                if (existingItem != null)
+                {
+                    existingItem.Quantity += quantity;
+                }
+                else
+                {
+                    var newItem = new ShopItem
+                    {
+                        ItemId = item.ItemId,
+                        ItemName = item.ItemName,
+                        Price = item.Price,
+                        Quantity = quantity
+                    };
+                    Items.Add(newItem);
+                }
+            }catch(Exception ex) {Console.WriteLine(ex.Message);}
+        }
 
 
 
