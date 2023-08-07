5e6c033b6367cbd8572382705ee4df85382b08d2
omranali2111 <aljabri1234@hotmail.com>
2023-08-07 13:25:04 +0400

    adding shopsetting class

--- InvoicingSystem/shopSetting.cs
+++ InvoicingSystem/shopSetting.cs
@@ -9,9 +9,47 @@ namespace InvoicingSystem
 {
     internal class shopSetting
     {
-        public string ShopName { set; get; }
-        public string InvoiceHeader { set; get; }
+        public string ShopName { get; set; }
+        public string InvoiceHeader { get; private set; }
+        public string Telephone { get; private set; }
+        public string Fax { get; private set; }
+        public string Email { get; private set; }
+        public string Website { get; private set; }
 
-       
+        public void SetInvoiceHeader(string header)
+        {
+            InvoiceHeader = header;
+        }
+
+        public void SetContactInformation(string telephone, string fax, string email, string website)
+        {
+            Telephone = telephone;
+            Fax = fax;
+            Email = email;
+            Website = website;
+        }
+
+        public void Save()
+        {
+            string json = JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText("ShopSettings.json", json);
+        }
+
+        public static shopSetting Load()
+        {
+            try
+            {
+                string json = File.ReadAllText("ShopSettings.json");
+                return JsonSerializer.Deserialize<shopSetting>(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while loading shop settings: {ex.Message}");
+                return new shopSetting();
+            }
+        }
     }
+
+
 }
+
